142466438fcf253c8577032e9f091ce0fb3faa85
Perry Brandiezs <pbrandiezs@gmail.com>
2018-07-10 20:26:23 -0600

    Initial Commit

---
+++ Ex12.py
@@ -0,0 +1,9 @@
+
+a = [5, 10, 15, 20, 25]
+
+newlist = []
+newlist.append(a[0])
+newlist.append(a[-1])
+
+print(newlist)
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python-ex12
+https://www.practicepython.org/exercise/2014/04/25/12-list-ends.html
